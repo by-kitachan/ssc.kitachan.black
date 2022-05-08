61a3d4e8e4a25ec6d31d7bb840dd9ecfdc9e0df4
oliver <minakawa.daiki.as@gmail.com>
2022-05-07 23:33:30 -0700

    add toggle

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import {
 import ImageUploading, { ErrorsType } from 'react-images-uploading';
 import { ImageListType } from 'react-images-uploading/dist/typings';
 import Script from 'next/script';
+import { Switch } from '@headlessui/react';
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ');
@@ -123,6 +124,87 @@ const WarningModal: React.VFC<{
     </Transition.Root>
   );
 };
+
+function Toggle({
+  text,
+  enabled,
+  setEnabled,
+}: {
+  text: string;
+  enabled: boolean;
+  setEnabled: (enabled: boolean) => void;
+}) {
+  return (
+    <Switch.Group as="div" className="flex items-center justify-between">
+      <span className="flex-grow flex flex-col">
+        <Switch.Label
+          as="span"
+          className="text-sm font-medium text-gray-900"
+          passive
+        >
+          {text}
+        </Switch.Label>
+      </span>
+      <Switch
+        checked={enabled}
+        onChange={setEnabled}
+        className={classNames(
+          enabled ? 'bg-indigo-600' : 'bg-gray-200',
+          'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+        )}
+      >
+        <span className="sr-only">Use setting</span>
+        <span
+          className={classNames(
+            enabled ? 'translate-x-5' : 'translate-x-0',
+            'pointer-events-none relative inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
+          )}
+        >
+          <span
+            className={classNames(
+              enabled
+                ? 'opacity-0 ease-out duration-100'
+                : 'opacity-100 ease-in duration-200',
+              'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="h-3 w-3 text-gray-400"
+              fill="none"
+              viewBox="0 0 12 12"
+            >
+              <path
+                d="M4 8l2-2m0 0l2-2M6 6L4 4m2 2l2 2"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <span
+            className={classNames(
+              enabled
+                ? 'opacity-100 ease-in duration-200'
+                : 'opacity-0 ease-out duration-100',
+              'absolute inset-0 h-full w-full flex items-center justify-center transition-opacity'
+            )}
+            aria-hidden="true"
+          >
+            <svg
+              className="h-3 w-3 text-indigo-600"
+              fill="currentColor"
+              viewBox="0 0 12 12"
+            >
+              <path d="M3.707 5.293a1 1 0 00-1.414 1.414l1.414-1.414zM5 8l-.707.707a1 1 0 001.414 0L5 8zm4.707-3.293a1 1 0 00-1.414-1.414l1.414 1.414zm-7.414 2l2 2 1.414-1.414-2-2-1.414 1.414zm3.414 2l4-4-1.414-1.414-4 4 1.414 1.414z" />
+            </svg>
+          </span>
+        </span>
+      </Switch>
+    </Switch.Group>
+  );
+}
 
 const Home: NextPage = () => {
   const [errors, setErrors] = useState<ErrorsType>();
@@ -268,7 +350,7 @@ const Home: NextPage = () => {
     intMat.delete();
     cv.imshow('dest-canvas', retMat);
     setCreated(true);
-  }, [images]);
+  }, [images, deleteScrollBar, deleteSideMargin]);
 
   const onDownload = useCallback(() => {
     const canvas = document.querySelector<HTMLCanvasElement>('#dest-canvas');
@@ -586,7 +668,26 @@ const Home: NextPage = () => {
           );
         }}
       </ImageUploading>
-      <div className="text-center mt-8">
+      <div className="my-6 lg:w-1/3 mx-auto p-6 border-2 border-dashed rounded-md">
+        <h2 className="mx-auto text-gray-500 border-gray-300 border-b">
+          オプション
+        </h2>
+        <div className="my-3 mx-auto">
+          <Toggle
+            text="左右を切り落とす"
+            enabled={deleteSideMargin}
+            setEnabled={setDeleteSideMargin}
+          />
+        </div>
+        <div className="mt-3 mx-auto">
+          <Toggle
+            text="スクロールバーを消去する"
+            enabled={deleteScrollBar}
+            setEnabled={setDeleteScrollBar}
+          />
+        </div>
+      </div>
+      <div className="text-center mt-4">
         <button
           onClick={onCreate}
           type="button"
